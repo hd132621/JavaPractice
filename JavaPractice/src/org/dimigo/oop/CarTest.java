188b01a66afb5c44361f7eecb233c957eebb9a82
ijaehyeog <ijaehyeog@172.17.0.210>
2015-04-16 09:27:57 +0900

    7-2과제 제출

--- JavaPractice/src/org/dimigo/oop/CarTest.java
+++ JavaPractice/src/org/dimigo/oop/CarTest.java
@@ -5,7 +5,7 @@ package org.dimigo.oop;
 public class CarTest {
 
 	public static void main(String[] args) {
-		Car car = new Car();
+		/*Car car = new Car();
 		Car car2 = new Car();
 		Car car3 = new Car();
 		System.out.println("<<자동차 목록>>");
@@ -13,19 +13,21 @@ public class CarTest {
 		car.setModel("제네시스");
 		car.setColor("검정색");
 		car.setMaxSpeed(225);
-		car.setPrice(50000000);
-		System.out.println("제조사명 : "+ car.getCompany());
-		System.out.println("모델명 : "+ car.getModel());
-		System.out.println("색상 : "+ car.getColor());
-		System.out.println("최대속도 : "+ car.getMaxSpeed()+ "km");
-		System.out.println("가격 : "+ String.format("%,d", car.getPrice())+"원");
+		car.setPrice(50000000);*/
+		Car2 car1 = new Car2("현대자동차", "제네시스", "검정색", 225, 50000000);
+		System.out.println("제조사명 : "+ car1.getCompany());
+		System.out.println("모델명 : "+ car1.getModel());
+		System.out.println("색상 : "+ car1.getColor());
+		System.out.println("최대속도 : "+ car1.getMaxSpeed()+ "km");
+		System.out.println("가격 : "+ String.format("%,d", car1.getPrice())+"원");
 		System.out.println("");
 		
-		car2.setCompany("기아자동차");
-		car2.setModel("K7");
-		car2.setColor("흰색");
-		car2.setMaxSpeed(246);
-		car2.setPrice(40000000);
+		//car2.setCompany("기아자동차");
+		//car2.setModel("K7");
+		//car2.setColor("흰색");
+		//car2.setMaxSpeed(246);
+		//car2.setPrice(40000000);
+		Car2 car2 = new Car2("기아자동차", "K7", "흰색", 246, 40000000);
 		System.out.println("제조사명 : "+ car2.getCompany());
 		System.out.println("모델명 : "+ car2.getModel());
 		System.out.println("색상 : "+ car2.getColor());
@@ -33,11 +35,12 @@ public class CarTest {
 		System.out.println("가격 : "+ String.format("%,d", car2.getPrice())+"원");
 		System.out.println("");
 		
-		car3.setCompany("삼성자동차");
-		car3.setModel("SM7");
-		car3.setColor("회색");
-		car3.setMaxSpeed(200);
-		car3.setPrice(38000000);
+		//car3.setCompany("삼성자동차");
+		//car3.setModel("SM7");
+		//car3.setColor("회색");
+		//car3.setMaxSpeed(200);
+		//car3.setPrice(38000000);
+		Car2 car3 = new Car2("삼성자동차", "SM7", "회색", 200, 38000000);
 		System.out.println("제조사명 : "+ car3.getCompany());
 		System.out.println("모델명 : "+ car3.getModel());
 		System.out.println("색상 : "+ car3.getColor());
