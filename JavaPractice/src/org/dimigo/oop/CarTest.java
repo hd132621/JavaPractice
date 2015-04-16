b83b3f2a1c12689edfe77cefb105c224144c3f74
ijaehyeog <ijaehyeog@172.17.0.210>
2015-04-16 09:54:44 +0900

    7-3 과제 제

--- JavaPractice/src/org/dimigo/oop/CarTest.java
+++ JavaPractice/src/org/dimigo/oop/CarTest.java
@@ -14,7 +14,7 @@ public class CarTest {
 		car.setColor("검정색");
 		car.setMaxSpeed(225);
 		car.setPrice(50000000);*/
-		Car2 car1 = new Car2("현대자동차", "제네시스", "검정색", 225, 50000000);
+		Car3 car1 = new Car3("현대자동차", "제네시스", "검정색", 225, 50000000);
 		System.out.println("제조사명 : "+ car1.getCompany());
 		System.out.println("모델명 : "+ car1.getModel());
 		System.out.println("색상 : "+ car1.getColor());
@@ -27,7 +27,7 @@ public class CarTest {
 		//car2.setColor("흰색");
 		//car2.setMaxSpeed(246);
 		//car2.setPrice(40000000);
-		Car2 car2 = new Car2("기아자동차", "K7", "흰색", 246, 40000000);
+		Car3 car2 = new Car3("기아자동차", "K7", "흰색", 246);
 		System.out.println("제조사명 : "+ car2.getCompany());
 		System.out.println("모델명 : "+ car2.getModel());
 		System.out.println("색상 : "+ car2.getColor());
@@ -40,7 +40,7 @@ public class CarTest {
 		//car3.setColor("회색");
 		//car3.setMaxSpeed(200);
 		//car3.setPrice(38000000);
-		Car2 car3 = new Car2("삼성자동차", "SM7", "회색", 200, 38000000);
+		Car3 car3 = new Car3("삼성자동차", "SM7", "회색", 200);
 		System.out.println("제조사명 : "+ car3.getCompany());
 		System.out.println("모델명 : "+ car3.getModel());
 		System.out.println("색상 : "+ car3.getColor());
