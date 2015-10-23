e6300b94e4744da50d66d5c5164bc5669042e84b
ijaehyeog <ijaehyeog@172.17.2.22>
2015-10-23 16:42:04 +0900

    io 실습과제1

---
+++ JavaPractice/src/org/dimigo/io/SaveImageFromUrl.java
@@ -0,0 +1,57 @@
+/**
+ * 
+ */
+package org.dimigo.io;
+
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+
+/**
+ * <pre>
+ * org.dimigo.io
+ * 		|_ SaveImageFromUrl
+ *  
+ * 1. 개요 :
+ * 2. 작성일 : 2015. 10. 23.
+ * </pre>
+ *
+ * @author ijaehyeog
+ * @version 1.0.0
+ * 
+ */
+public class SaveImageFromUrl {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		String imageUrl = "https://static.dimigo.in/img/logobeta.png";
+		
+		try{
+			URL url = new URL(imageUrl);
+		
+		try(InputStream is = url.openStream();
+				OutputStream os = new FileOutputStream("files/logo.png")){
+			
+			int result;
+			
+			byte buf[] = new byte[100];
+			
+			while((result=is.read(buf))!=-1){
+				os.write(buf,0,result);
+			}
+			
+			System.out.println("파일 생성 완료!");
+			
+			
+		}
+		}catch (Exception e){
+			e.printStackTrace();
+		}
+
+	}
+
+}
